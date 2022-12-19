3334010dc6725c7f60a6a6903ec55c11bf31ce7b
Ty Martz <jacksontmartz@gmail.com>
2022-12-19 17:10:02 -0500

    working api with cycle, recovery, sleep

--- whoopAPI.py
+++ whoopAPI.py
@@ -1,32 +1,29 @@
 import requests
 import requests.auth
 import os
-
 from dotenv import load_dotenv
-
 load_dotenv()
 
 CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
-
-# TODO: add in credential request for tokens
-
 USERNAME = os.environ.get("MY_WHOOP_EMAIL")
 PW = os.environ.get("MY_WHOOP_PW")
-TOKEN_URL = "https://api.prod.whoop.com/oauth/oauth2/token"
+#TOKEN_URL = "https://api.prod.whoop.com/oauth/oauth2/token"
+TOKEN_URL = "https://api-7.whoop.com/oauth/token"
 
 
-def get_token(username, password, token_url):
+def get_token(username, pw, token_url):
+    print('')
     r = requests.post(token_url, json={
+        "username": username,
+        "password": pw,
         "grant_type": "password",
-        "issueRefresh": False,
-        "password": password,
-        "username": username
-    })
-
+        "issueRefresh": True,
+    })    
     # Exit if fail
     if r.status_code != 200:
         print("Fail - Credentials rejected.")
+        print('')
         exit()
     else:
         print("Success - Credentials accepted")
@@ -42,6 +39,22 @@ user_id, access_token = get_token(USERNAME, PW, TOKEN_URL)
 headers = {
     'Authorization': 'bearer {}'.format(access_token)
 }
-recov = requests.get("https://api.prod.whoop.com/developer/v1/recovery", headers=headers).json()
-for k,v in recov['records'][0].items():
+print('')
+print('-- CYCLE --')
+cycle = requests.get("https://api.prod.whoop.com/developer/v1/cycle", headers=headers).json()
+for k,v in cycle['records'][0].items():
+    print(f"{k} --> {v}")
+print('')
+
+print('-- RECOVERY --')
+recov = requests.get(f"https://api.prod.whoop.com/developer/v1/cycle/{cycle['records'][0]['id']}/recovery", headers=headers).json()
+for k,v in recov.items():
+    print(f"{k} --> {v}")
+print('')
+
+print('-- SLEEP --')
+sleep = requests.get(f"https://api.prod.whoop.com/developer/v1/activity/sleep/{recov['sleep_id']}",
+                     headers=headers).json()
+
+for k,v in sleep.items():
     print(f"{k} --> {v}")
